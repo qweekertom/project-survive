4ef8631e3c177855cf9bb03ef1b3714f1194c65d
Jack Chambers <jtcroxley@gmail.com>
2020-11-04 16:37:20 -0500

    aero added

---
+++ src/Client/Controllers/ToolController.lua
@@ -0,0 +1,20 @@
+-- Tool Controller
+-- qweekertom
+-- November 4, 2020
+
+
+
+local ToolController = {}
+
+
+function ToolController:Start()
+	
+end
+
+
+function ToolController:Init()
+	
+end
+
+
+return ToolController
